42f4dbda1dc74c7b153e37069f0dedbe25eb4ef5
kaffu0424 <kaffu@naver.com>
2024-03-05 20:18:26 +0900

    수동 merge

---
+++ Assets/Script/Housing/HousingManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HousingManager : MonoBehaviour
+{
+    [SerializeField]
+    Canvas _craftCanvas;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(1))        // 우클릭을 하고 있는 동안
+            F_OnOffCraftCanvas(true);       // canvas 보이게
+        else if (Input.GetMouseButtonUp(1)) // 우클릭 떼면
+            F_OnOffCraftCanvas(false);      // cavas 안보이게
+
+    }
+    public void F_OnOffCraftCanvas(bool v_check)
+    {
+        _craftCanvas.gameObject.SetActive(v_check);
+        F_SetMouseMove(v_check);
+    }
+
+    public void F_SetMouseMove(bool v_mode)
+    {
+        if (!v_mode)
+        {
+            Cursor.lockState = CursorLockMode.Locked;        // 커서를 '화면 정중앙'에 고정시킴
+            Cursor.visible = false;                          // 커서 안 보이게
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;             // 커서 원래대로
+            Cursor.visible = true;                              // 커서 보이게
+        }
+    }
+}
