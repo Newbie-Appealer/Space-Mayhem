98334237af826a66d3f2ea6111ba2bf355c993ef
kaffu0424 <kaffu@naver.com>
2024-03-06 16:49:19 +0900

    스크랩 생성

---
+++ Assets/Script/Fiber.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fiber : Etc_Item
+{
+    private void Awake()
+    {
+        SetEtcItemName("Fiber");
+    }
+}
---
+++ Assets/Script/Scrap.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Scrap : Etc_Item
+{
+    private void Awake()
+    {
+        SetEtcItemName("Scrap");
+    }
+}
