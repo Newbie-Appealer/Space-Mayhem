1cf5127b867d5ce14463da735d64d213f7f2d64f
kaffu0424 <kaffu@naver.com>
2024-03-06 13:48:55 +0900

    Revert "Merge pull request #6 from Newbie-Appealer/kaffu_0306_1"
    This reverts commit 0e842b15423479324aaeabfd56ba0be0105b7728, reversing
    changes made to 42f4dbda1dc74c7b153e37069f0dedbe25eb4ef5.

--- Assets/Script/Items/InventorySystem.cs
+++ Assets/Script/Items/InventorySystem.cs
@@ -3,7 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventorySystem : MonoBehaviour
+public class InventorySystem : Singleton<InventorySystem>
 {
-
+    protected override void InitManager()
+    {
+        
+    }
 }
