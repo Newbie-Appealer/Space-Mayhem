27520c007ae32aca368bc6d7ffd8db4b219aa810
Dayo <dmschd0817@naver.com>
2024-03-07 15:44:59 +0900

    운석 및 아이템 수정

--- Assets/Dayo_Folder/MeteorManager.cs
+++ Assets/Dayo_Folder/MeteorManager.cs
@@ -7,14 +7,15 @@ public class MeteorManager : Singleton<MeteorManager>
 {
     [Header("운석 정보")]
     [SerializeField]       private GameObject _Meteor_Object; // 운석 원본 프리팹
-    [Range(1f, 4f)]         public float _Meteor_Delay; //운석 떨어지게 할 딜레이
-    [Range(300f, 500f)] public float _Meteor_Distance = 300f; //플레이어 주변 범위랑 운석 사이 거리
-    [SerializeField]       private float _Meteor_Count; //운석 총 개수
+    [Range(1f, 4f)]         public float _Meteor_Delay;                 //운석 떨어지게 할 딜레이
+    [Range(300f, 500f)] public float _Meteor_Distance = 200f; //플레이어 주변 범위랑 운석 사이 거리
     [SerializeField]       private float _Meteor_Spawn_SphereRange = 200f; // 운석 생성 최대 범위 원 반지름
+    [SerializeField]       private int _Meteor_Count; //운석 초기 풀링 개수
+    GameObject _Meteor_Group;              //운석 스폰 포인트 모아둘 빈 오브젝트
 
     [Header("풀링")]
     private Queue<GameObject> _Pooling_Meteor;               //메테오 풀링
-    private Queue<Transform>     _Pooling_MeteorSpawner; //메테오 스포너 풀링
+    private List<Vector3>     _Pooling_MeteorSpawner;          //메테오 스폰 위치
 
     [Header("플레이어")]
     //플레이어 주변 범위 원 범위 (Player 오브젝트 밑에 새 오브젝트 추가해서 SphereCollider 생성 및 Radius 10으로 생성해주세요)
@@ -26,35 +27,39 @@ public class MeteorManager : Singleton<MeteorManager>
     protected override void InitManager()
     {
         _Pooling_Meteor = new Queue<GameObject>();
-        _Pooling_MeteorSpawner = new Queue<Transform>();
+        _Pooling_MeteorSpawner = new List<Vector3>();
 
-        //스폰 포인트 및 운석 개수만큼
+        _Meteor_Group = new GameObject();
+        _Meteor_Group.name = "Meteor_Group";
+        _Meteor_Group.transform.position = Vector3.zero;
+        
+        //원하는 개수만큼 스폰 포인트 및 운석 개수 조정
         for (int l = 0; l < _Meteor_Count; l++) 
         {
-            F_CreateMeteor(F_CreateMeteorSpawnPoint());
+            F_CreateMeteorSpawnPoint();
+            F_CreateMeteor(l);
         }
         StartCoroutine(C_MeteorSpawn());
     }
 
-    //운석 풀링
-    private void F_CreateMeteor(Transform v_SpanwerTransform)
+    //운석 스폰 포인트 풀링
+    private Vector3 F_CreateMeteorSpawnPoint()
     {
-        GameObject _Meteor = Instantiate(_Meteor_Object, v_SpanwerTransform.position, Quaternion.identity);
-        _Meteor.transform.SetParent(v_SpanwerTransform);
-        _Meteor.name = "Meteor";
-        _Pooling_Meteor.Enqueue( _Meteor );
-        _Meteor.SetActive(false);
+        Vector3 _randomSpawner = Random.onUnitSphere * _Meteor_Spawn_SphereRange;
+        _Pooling_MeteorSpawner.Add(_randomSpawner);
+        return _randomSpawner;
     }
 
-    //운석 스폰 포인트 풀링
-    private Transform F_CreateMeteorSpawnPoint()
+    //운석 풀링
+    private void F_CreateMeteor(int v_Index)
     {
-        GameObject _MeteorSpawner = new GameObject();
-        Vector3 _randomSpawner = Random.onUnitSphere * _Meteor_Spawn_SphereRange;
-        _MeteorSpawner.transform.position = _randomSpawner;
-        _Pooling_MeteorSpawner.Enqueue(_MeteorSpawner.transform);
-        _MeteorSpawner.name = "MeteorSpawner";
-        return _MeteorSpawner.transform;
+        GameObject _Meteor = Instantiate(_Meteor_Object);
+        _Meteor.name = "Meteor";
+        _Meteor.transform.SetParent(_Meteor_Group.transform);
+        Vector3 _spawn_Point = _Pooling_MeteorSpawner[v_Index];
+        _Meteor.transform.position = _spawn_Point;
+        _Pooling_Meteor.Enqueue( _Meteor );
+        _Meteor.SetActive(false);
     }
 
     //Delay마다 1개씩 운석 Dequeue
@@ -64,20 +69,26 @@ public class MeteorManager : Singleton<MeteorManager>
         {
             while (_Pooling_Meteor.Count > 0)
             {
-                GameObject _spawnedMeteor = _Pooling_Meteor.Dequeue();
-                _spawnedMeteor.SetActive(true);
+                F_MeteorSpawn();
                 yield return new WaitForSeconds(_Meteor_Delay);
             }
-            //풀링에 더 이상 운석이 없다면 새로운 스폰 포인트 및 운석 생성
-            F_CreateMeteor(F_CreateMeteorSpawnPoint());
+            //풀링에 운석이 없다면 새로운 스폰 포인트 및 운석 생성
+            F_CreateMeteorSpawnPoint();
+            F_CreateMeteor(_Meteor_Count);
+            _Meteor_Count++;
+            F_MeteorSpawn();
             yield return new WaitForSeconds(3f);
         }
     }
 
+    private void F_MeteorSpawn()
+    {
+        GameObject _spawnedMeteor = _Pooling_Meteor.Dequeue();
+        _spawnedMeteor.SetActive(true);
+    }
     //삭제된 메테오 풀링
     public void F_MeteorPoolingAdd(GameObject v_DestroyedMeteor)
     {
-        Debug.Log("메테오 풀링 중");
         _Pooling_Meteor.Enqueue(v_DestroyedMeteor);
     }
 }
--- Assets/Script/Scraps/Scrap.cs
+++ Assets/Script/Scraps/Scrap.cs
@@ -6,36 +6,40 @@ public class Scrap : MonoBehaviour
 {
     [Header("Setting")]
     [SerializeField] private string _itemName;
+    private Vector3 _etcItem_StartPosition;
 
-    // 재료 아이템 정보
-    [Header("아이템 정보")]
-    private GameObject _etcItem_Object;
-    protected string _etcItem_Name;
-    private Transform _etcItem_StartPosition;
     private Rigidbody _rb;
+
     protected void Start()
     {
         //생성 위치 : 스폰 포인트 위치
-        _etcItem_StartPosition = transform.parent.transform;
+        _etcItem_StartPosition = transform.position;
 
-        //최초 생성 시 초기화 (오브젝트, 이름, 생성 위치)
-        F_InitializeItem(this.gameObject, _etcItem_Name, _etcItem_StartPosition);
+        //최초 생성 시 초기화 (이름, 생성 위치)
+        F_InitializeItem(_itemName, _etcItem_StartPosition);
 
         //아이템 생성 시 이동
         _rb = GetComponent<Rigidbody>();
         _rb.velocity = new Vector3(ScrapManager.Instance._item_MoveSpeed, 0, 0);
 
-        //시작과 동시에 거리 체크 코루틴 실행
-        StartCoroutine(C_ItemDistanceCheck(_etcItem_Object));
     }
 
-    // 오브젝트 초기화 함수 (이름, 맨 처음 생성된 위치)
-    // 위치를 다시 랜덤하게 해서 다시 생성하게 해야할 지 고민중
-    private void F_InitializeItem(GameObject v_GameObject, string v_Name, Transform v_Trans)
+    protected void OnEnable()
     {
-        _etcItem_Object = v_GameObject;
-        _etcItem_Object.name = v_Name;
-        _etcItem_StartPosition = v_Trans;
+        //활성화와 동시에 거리 체크 코루틴 실행
+        StartCoroutine(C_ItemDistanceCheck(gameObject));
+    }
+    //오브젝트 생성 위치 가져오기
+    public Vector3 F_SaveStartPoint(Vector3 v_point)
+    {
+        _etcItem_StartPosition = v_point;
+        return _etcItem_StartPosition;
+    }
+    // 오브젝트 초기화 함수 (이름, 맨 처음 생성된 위치)
+    private void F_InitializeItem(string v_Name, Vector3 v_Trans)
+    {
+        gameObject.name = v_Name;
+        transform.position = v_Trans;
     }
 
     private IEnumerator C_ItemDistanceCheck(GameObject v_etcItem)
@@ -44,7 +48,6 @@ public class Scrap : MonoBehaviour
         Transform _playerTrs;
 
         //아이템이 활성화돼있는 동안 3초에 한번씩 실행 
-        // => 따라서 Player와의 거리 수치를 높여도 바로 반영되지 않음. 약간 느리게 반영되는 듯함.
         while (v_etcItem.activeSelf)
         {
             _playerTrs = ScrapManager.Instance.playerTransform;
@@ -52,10 +55,9 @@ public class Scrap : MonoBehaviour
             if (Vector3.Distance(v_etcItem.transform.position, _playerTrs.position) > _distance)
             {
                 gameObject.SetActive(false);
-                F_InitializeItem(_etcItem_Object, _etcItem_Name, _etcItem_StartPosition);                  // 오브젝트 초기화
-                ScrapManager.Instance.F_ItemPoolingAdd(_etcItem_Object);                                  // 풀링에 추가
+                F_InitializeItem(_itemName, _etcItem_StartPosition);                  // 오브젝트 초기화
+                ScrapManager.Instance.F_ItemPoolingAdd(v_etcItem);                                  // 풀링에 추가
                 StopAllCoroutines();
-
             }
             yield return new WaitForSeconds(3f);
         }
