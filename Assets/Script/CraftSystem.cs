53b4a2bcb8ca1f7936641f925489f834936ce360
kaffu0424 <kaffu@naver.com>
2024-03-11 20:44:50 +0900

    UIManager, InputSystem, CraftSystem

---
+++ Assets/Script/CraftSystem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum installation
+{
+    NONE,       // n
+    TEST
+}
+
+public struct Recipe
+{
+    public int _itemCode;                       // 제작 아이템 번호
+    public ItemType _itemType;                  // 제작 아이템 타입
+    public installation _need_Installation;     // 제작 활성화에 필요한 설치물
+
+    public int[] _recipeCode;                   // 제작에 필요한 아이템 번호
+    public int[] _recipeCount;                  // 제작에 필요한 아이템 개수
+}
+public class CraftSystem : MonoBehaviour
+{
+    [SerializeField] private List<Recipe> _recipes;
+}
