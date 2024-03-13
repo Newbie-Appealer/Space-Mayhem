7cd5baafd975e2a4a8245911f228aee486720f24
binanuku <binanuku@gmail.com>
2024-03-14 02:12:52 +0900

    행성 생성 및 움직이는 기능 구현

---
+++ Assets/Script/PlanetController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlanetController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _planetPrefabs;
+    [SerializeField] private Transform _createPosition;
+    [SerializeField] private float _currentTime;
+    private int _planetIndex;
+
+    private void Update()
+    {
+        F_CreatePlanet();
+    }
+
+    public void F_CreatePlanet()
+    {
+        _currentTime += Time.deltaTime;
+
+        if (_currentTime >= 5f && _planetIndex < _planetPrefabs.Length)
+        {
+            GameObject _planet = Instantiate(_planetPrefabs[_planetIndex], _createPosition.position, Quaternion.identity);
+            _planet.transform.SetParent(_createPosition);
+            StartCoroutine(F_MovePlanet(_planet));
+            _planetIndex++;
+            _currentTime = Time.deltaTime;
+        }
+    }
+
+
+    IEnumerator F_MovePlanet(GameObject v_planet)
+    {
+        Rigidbody rb = v_planet.GetComponent<Rigidbody>();
+        rb.velocity = Vector3.right * 25;
+        yield return null;
+    }
+}
