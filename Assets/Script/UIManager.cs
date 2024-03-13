1b40bfa707c31c46ca54e6a24c193b7360b6961a
Dayo <dmschd0817@naver.com>
2024-03-13 14:18:14 +0900

    머지? 머지?

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : Singleton<UIManager>
 {
     [SerializeField] private GameObject _inventoryUI;
     [SerializeField] private Canvas _canvas;
+    // 0 : 산소 , 1 : 물 , 2 : 배고픔
+    [SerializeField] private Image[] _player_StatUI;
     public Canvas canvas => _canvas;
-    protected override void InitManager() { }
+    protected override void InitManager() 
+    {
+    }
 
     public void F_InventoryUI()
     {
@@ -24,5 +29,12 @@ public class UIManager : Singleton<UIManager>
             ItemManager.Instance.inventorySystem.F_InventoryUIUpdate(); // 인벤토리 업데이트
             _inventoryUI.SetActive(true);                               // 인벤토리 ON
         }
+    }
+
+    public void F_PlayerStatUIUpdate()
+    {
+        _player_StatUI[0].fillAmount = PlayerManager.Instance.F_GetStat(0) / 100f ;
+        _player_StatUI[1].fillAmount = PlayerManager.Instance.F_GetStat(1) / 100f;
+        _player_StatUI[2].fillAmount = PlayerManager.Instance.F_GetStat(2) / 100f;
     }
 }
