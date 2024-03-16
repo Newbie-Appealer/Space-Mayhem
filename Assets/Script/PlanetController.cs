9024425d137fd4b28550cf18dbb711bff22c208c
binanuku <binanuku@gmail.com>
2024-03-16 23:06:58 +0900

    행성 오브젝트 수정

--- Assets/Script/PlanetController.cs
+++ Assets/Script/PlanetController.cs
@@ -10,11 +10,7 @@ public class PlanetController : MonoBehaviour
     [SerializeField] private Transform _createPosition;
     [SerializeField] private float _currentTime;
     [SerializeField] private GameObject _teleport;
-    private int _planetIndex;
-
-    private void Awake()
-    {
-    }
+    public int _planetIndex;
 
     private void Update()
     {
@@ -26,7 +22,7 @@ public class PlanetController : MonoBehaviour
     {
         _currentTime += Time.deltaTime; //계속 진행되는 시간
 
-        if (_currentTime >= 5f && _planetIndex < _planetPrefabs.Length)
+        if (_currentTime >= 30f && _planetIndex < _planetPrefabs.Length)
         {
             GameObject _planet = Instantiate(_planetPrefabs[_planetIndex], _createPosition.position, Quaternion.identity); //행성 생성
             _planet.transform.SetParent(_createPosition); //생성 위치의 자식으로 지정
@@ -34,11 +30,10 @@ public class PlanetController : MonoBehaviour
             Rigidbody _planetRb = _planet.GetComponent<Rigidbody>();
             _planetRb.velocity = Vector3.right * 500;
 
-            StartCoroutine(F_DestroyPlanet(_planetRb)); //행성 움직이기
+            _teleport.SetActive(true); //텔레포트 생성은 행성이 시야에 보일 때 나오는 걸로 바꾸기
+            StartCoroutine(F_DestroyPlanet(_planetRb)); //행성 삭제 체크
 
             _currentTime = Time.deltaTime; //행성 생성 후 시간 초기화
-            _teleport.SetActive(true); //텔레포트 생성
-            _planetIndex++;
         }
     }
 
@@ -49,9 +44,12 @@ public class PlanetController : MonoBehaviour
             if (_planetRb.position.x >= 1500) //행성의 x값이 1500이 넘으면
             {
                 Destroy(_planetRb.gameObject); //행성 삭제
+                _teleport.SetActive(false); //텔레포트 삭제
+                _teleport.GetComponent<SceneTrigger>()._teleportUI.SetActive(false);
                 break;
             }
             yield return new WaitForSeconds(1f);
         }
+        _planetIndex++;
     }
 }
