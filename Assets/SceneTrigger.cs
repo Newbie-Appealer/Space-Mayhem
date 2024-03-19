518bf6acc20293d56245ff839428b00ab2ae60a8
Da Hoon <binanuku@gmail.com>
2024-03-19 19:57:28 +0900

    위치 복귀 및 씬 언로드

--- Assets/SceneTrigger.cs
+++ Assets/SceneTrigger.cs
@@ -6,11 +6,12 @@ using UnityEngine.SceneManagement;
 public class SceneTrigger : MonoBehaviour
 {
     [SerializeField] private string[] _targetScene;
-    Transform _playerPos;
+    [SerializeField] GameObject _playerPos;
     [SerializeField] LayerMask _layerMask;
     [SerializeField] public GameObject _teleportUI;
     [SerializeField] Camera _playerCam;
     PlanetController _planetController;
+    [SerializeField] Vector3[] _teleportPos;
     int idx;
     private void Awake()
     {
@@ -22,8 +23,13 @@ public class SceneTrigger : MonoBehaviour
         var scene = SceneManager.GetSceneByName(_targetScene[idx]); //불러올 씬 이름으로 가져오기
         if (!scene.isLoaded) //이미 로딩된 상태가 아닌지 검사
         {
+            _planetController._createFlag = false;
+            _planetController._planetRb.velocity = Vector3.zero;
+            _playerPos.transform.position = new Vector3(0, 1000, 0);
+            _planetController._teleport.transform.position = _teleportPos[idx];
+
             var op = SceneManager.LoadSceneAsync(_targetScene[idx], LoadSceneMode.Additive);
-            //비동기 방식으로 씬 불러오기
+            //비동기 방식으로 씬 로드
             //Additive = 다른 씬을 불러오면 원래 씬의 오브젝트 사라져서 문제가 발생할 수 있음
             while (!op.isDone)
             {
@@ -35,12 +41,16 @@ public class SceneTrigger : MonoBehaviour
     private IEnumerator UnloadStreamingScene()
     {
         var scene = SceneManager.GetSceneByName(_targetScene[idx]); //불러올 씬 이름으로 가져오기
-        if (scene.isLoaded) //이미 로딩된 상태가 아닌지 검사
+        if (scene.isLoaded) //이미 로딩된 상태인지 검사
         {
-            var op = SceneManager.LoadSceneAsync(_targetScene[idx], LoadSceneMode.Additive);
-            //비동기 방식으로 씬 불러오기
-            //Additive = 다른 씬을 불러오면 원래 씬의 오브젝트 사라져서 문제가 발생할 수 있음
-            while (!op.isDone)
+            _planetController._createFlag = true;
+            _planetController._planetRb.velocity = Vector3.right * 500;
+            _playerPos.transform.position = Vector3.zero;
+            _planetController._teleport.transform.position = new Vector3(0, 0.3f, 0);
+
+            var op = SceneManager.UnloadSceneAsync(_targetScene[idx]);
+            //비동기 방식으로 씬 언로드
+            while (op.isDone)
             {
                 yield return null;
             }
@@ -54,14 +64,13 @@ public class SceneTrigger : MonoBehaviour
     }
     public void F_Teleport()
     {
-        _playerPos = PlayerManager.Instance.playerTransform;
         if (Physics.Raycast(_playerCam.transform.position, _playerCam.transform.forward, 5f, _layerMask))
         {
             _teleportUI.SetActive(true);
             if (Input.GetKeyDown(KeyCode.E))
             {
                 StartCoroutine(StreamingScene());
-                _playerPos.position = new Vector3(0, 1000, 0);
+                StartCoroutine(UnloadStreamingScene());
             }
         }
         else
