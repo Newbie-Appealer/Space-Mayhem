4c5dfdc5ab726c6dada21d62264c1b96a568a80c
kaffu0424 <kaffu@naver.com>
2024-03-19 19:00:48 +0900

    제작 UI 프리팹 수정

---
+++ Assets/InstallSystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstallSystem : MonoBehaviour
+{
+    [Header("object prefabs")]
+    [SerializeField] private GameObject _prefabs;
+    [SerializeField] MeshRenderer _meshRenderer;
+    LayerMask _floorlayer;
+
+    public void F_OnIsntallMode(int v_objectcode)
+    {
+        
+    }
+}
