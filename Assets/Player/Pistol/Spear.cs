ef9aefd61da2626ee085bb7db270cc7669fa1e9c
Dayo <dmschd0817@naver.com>
2024-03-27 18:34:17 +0900

    플레이어 파밍총 구현 中

--- Assets/Player/Pistol/Spear.cs
+++ Assets/Player/Pistol/Spear.cs
@@ -1,7 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Spear : MonoBehaviour
 {
+    [SerializeField] private Transform _spear_Tail;
+    public Transform tail => _spear_Tail;
+    [SerializeField] private Transform _pistol_Muzzle;
+    public Transform muzzle => _pistol_Muzzle;
+    [SerializeField] private LineRenderer _line;
+
+    public void F_EnableLine()
+    {
+        _line.enabled = true;
+        _line.positionCount = 2;
+        _line.SetPosition(0, _pistol_Muzzle.position);
+        _line.SetPosition(1, _spear_Tail.position);
+    }
+
+    public void F_DisableLine()
+    {
+        _line.enabled = false;
+    }
+
+    public void F_RestoreLine()
+    {
+        _line.SetPosition(0, _pistol_Muzzle.position);
+        _line.SetPosition(1, _spear_Tail.position);
+    }
 }
