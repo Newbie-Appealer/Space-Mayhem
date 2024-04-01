be97131ab845b86e2603602785d6308b84331bfa
binanuku <binanuku@gmail.com>
2024-04-01 23:43:05 +0900

    설치 아이템 충돌 검사

---
+++ Assets/Install_Item.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Install_Item : MonoBehaviour
+{
+    public bool _checkInstall;
+    MeshRenderer _installMesh;
+    [SerializeField] Material _redColor;
+    [SerializeField] Material _greenColor;
+
+    private void Start()
+    {
+        _checkInstall = true;
+        _installMesh = GetComponent<MeshRenderer>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        _checkInstall = false;
+        _installMesh.material = _redColor;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<MeshRenderer>().material = _redColor;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _checkInstall = true;
+        _installMesh.material = _greenColor;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<MeshRenderer>().material = _greenColor;
+        }
+    }
+}
