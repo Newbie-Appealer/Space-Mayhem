639c8a090642dffe3b9df207cc9659bdc05c9782
Dayo <dmschd0817@naver.com>
2024-04-03 18:50:33 +0900

    메테오 리팩토링 中

--- Assets/Dayo_Folder/Meteor/Meteor.cs
+++ Assets/Dayo_Folder/Meteor/Meteor.cs
@@ -5,36 +5,34 @@ using UnityEngine;
 
 public class Meteor : MonoBehaviour
 {
+    [Header("=== ABOUT METEOR ===")]
+    [SerializeField] private float _meteor_MoveSpeed;
+    [SerializeField, Range(300f, 500f)] private float _meteor_Distance = 250f;
     private Rigidbody _rb;
-
-    [SerializeField] private float _meteor_MoveSpeed; //운석 속도
     private Vector3 _meteor_StartPosition;
-    
-    //타겟 좌표
+    public Vector3 MeteorStart => _meteor_StartPosition;
+
     private float _targetX; 
     private float _targetY;
     private float _targetZ;
 
     //플레이어 주변 범위 구체
     private float _player_Sphere_Radius;
-
-    private void Start()
+    public void F_SettingMeteor()
     {
         _rb = GetComponent<Rigidbody>();
         _player_Sphere_Radius = MeteorManager.Instance.player_SphereCollider.radius;
-        _meteor_StartPosition = transform.position;
-        F_InitializeMeteor(_meteor_StartPosition);
-        F_MoveMeteor(); 
+        gameObject.name = "Meteor";
     }
 
-    private void OnEnable()
+    public void F_InitializeMeteor(Vector3 v_Trans)
     {
-        //재료 아이템과 마찬가지로 코루틴으로 거리 측정
-        StartCoroutine(C_MeteorDistanceCheck(gameObject));
+        _meteor_StartPosition = v_Trans;
+        transform.position = _meteor_StartPosition;
+        gameObject.SetActive(false);
     }
 
-    //메테오 이동
-    private void F_MoveMeteor()
+    public void F_MoveMeteor()
     {
         // 플레이어 주변 구를 기준으로 그 안의 범위 랜덤한 좌표 뽑기
         float _targetX = Random.Range(-_player_Sphere_Radius, _player_Sphere_Radius);
@@ -42,25 +40,19 @@ public class Meteor : MonoBehaviour
         float _targetZ = Random.Range(-_player_Sphere_Radius, _player_Sphere_Radius);
         Vector3 _targetDirection = (new Vector3(_targetX, _targetY, _targetZ) - transform.position).normalized;
         _rb.velocity = _targetDirection * _meteor_MoveSpeed;
-    }
-
-    //메테오 초기화
-    private void F_InitializeMeteor(Vector3 v_Trans)
-    {
-        transform.position = v_Trans;
+        StartCoroutine(C_MeteorDistanceCheck(gameObject));
     }
 
     //메테오와 플레이어 주변 원 사이 거리 측정
     private IEnumerator C_MeteorDistanceCheck(GameObject v_Meteor) 
     {
-        float _distance = MeteorManager.Instance.F_GetMeteorDistance();
         Transform _PlayerSphere;
         while(gameObject.activeSelf)
         {
             _PlayerSphere = MeteorManager.Instance.player_SphereCollider.transform;
-            if (Vector3.Distance(_PlayerSphere.position, v_Meteor.transform.position) > _distance)
+            if (Vector3.Distance(_PlayerSphere.position, v_Meteor.transform.position) > _meteor_Distance)
             {
-                F_MeteorDestoryed();
+                F_InitializeMeteor(_meteor_StartPosition);
             }
             yield return new WaitForSeconds(3f);
         }
@@ -69,14 +61,8 @@ public class Meteor : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log(collision.gameObject.name);
-        F_MeteorDestoryed();
+        F_InitializeMeteor(_meteor_StartPosition);
+        MeteorManager.Instance.F_ReturnMeteor(this);
     }
 
-    private void F_MeteorDestoryed()
-    {
-        StopAllCoroutines();
-        F_InitializeMeteor(_meteor_StartPosition);
-        gameObject.SetActive(false);
-        MeteorManager.Instance.F_MeteorPoolingAdd(this.gameObject);
-    }
 }
--- Assets/Dayo_Folder/Meteor/MeteorManager.cs
+++ Assets/Dayo_Folder/Meteor/MeteorManager.cs
@@ -10,11 +10,10 @@ public class MeteorManager : Singleton<MeteorManager>
     [SerializeField]  private float _meteor_Spawn_SphereRange = 200f; // 운석 생성 최대 범위 원 반지름
     [SerializeField]  private int _meteor_Count; //운석 초기 풀링 개수
     [SerializeField, Range(1f, 4f)] private float _meteor_Delay;                 //운석 떨어지게 할 딜레이
-    [SerializeField, Range(300f, 500f)] private float _meteor_Distance = 200f; //플레이어 주변 범위랑 운석 사이 거리
     private GameObject _meteor_Group;              //운석 스폰 포인트 모아둘 빈 오브젝트
 
     [Header("풀링")]
-    private Queue<GameObject> _pooling_Meteor;               //메테오 풀링
+    private Queue<Meteor> _pooling_Meteor;               //메테오 풀링
     private List<Vector3>     _pooling_MeteorSpawner;          //메테오 스폰 위치
 
     [Header("플레이어")]
@@ -26,12 +25,12 @@ public class MeteorManager : Singleton<MeteorManager>
 
     protected override void InitManager()
     {
-        _pooling_Meteor = new Queue<GameObject>();
+        _pooling_Meteor = new Queue<Meteor>();
         _pooling_MeteorSpawner = new List<Vector3>();
 
         _meteor_Group = new GameObject();
         _meteor_Group.name = "Meteor_Group";
-        _meteor_Group.transform.position = Vector3.zero;
+        _meteor_Group.transform.position = new Vector3(-10, 8.4f, 19f);
         
         //원하는 개수만큼 스폰 포인트 및 운석 개수 조정
         for (int l = 0; l < _meteor_Count; l++) 
@@ -53,13 +52,13 @@ public class MeteorManager : Singleton<MeteorManager>
     //운석 풀링
     private void F_CreateMeteor(int v_Index)
     {
-        GameObject _Meteor = Instantiate(_meteor_Object);
+        Meteor _Meteor = Instantiate(_meteor_Object).GetComponent<Meteor>();
         Vector3 _spawn_Point = _pooling_MeteorSpawner[v_Index];
         _Meteor.transform.SetParent(_meteor_Group.transform);
-        _Meteor.transform.position = _spawn_Point;
-        _Meteor.name = "Meteor";
+        _Meteor.F_SettingMeteor();
+        _Meteor.F_InitializeMeteor(_spawn_Point);
+
         _pooling_Meteor.Enqueue( _Meteor );
-        _Meteor.SetActive(false);
     }
 
     //Delay마다 1개씩 운석 Dequeue
@@ -83,17 +82,14 @@ public class MeteorManager : Singleton<MeteorManager>
 
     private void F_MeteorSpawn()
     {
-        GameObject _spawnedMeteor = _pooling_Meteor.Dequeue();
-        _spawnedMeteor.SetActive(true);
+        Meteor _spawnedMeteor = _pooling_Meteor.Dequeue();
+        _spawnedMeteor.gameObject.SetActive(true);
+        _spawnedMeteor.F_MoveMeteor();
     }
     //삭제된 메테오 풀링
-    public void F_MeteorPoolingAdd(GameObject v_DestroyedMeteor)
+    public void F_ReturnMeteor(Meteor v_DestroyedMeteor)
     {
         _pooling_Meteor.Enqueue(v_DestroyedMeteor);
-    }
-
-    public float F_GetMeteorDistance()
-    {
-        return _meteor_Distance;
+        v_DestroyedMeteor.F_InitializeMeteor(v_DestroyedMeteor.MeteorStart);
     }
 }
