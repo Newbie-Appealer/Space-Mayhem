fb8f50a8098ae5d4f575d1222e09fd4389e1a0d5
kaffu0424 <kaffu@naver.com>
2024-04-09 19:35:15 +0900

    인벤토리/하우징 저장/불러오기 DB 적용

--- Assets/Script/DataBase/AccountManager.cs
+++ Assets/Script/DataBase/AccountManager.cs
@@ -13,6 +13,9 @@ public class AccountManager : Singleton<AccountManager>
     [SerializeField] private string _playerPW = string.Empty;
     [SerializeField] private int _playerUID = -1;
 
+    public bool isLocal => _playerUID == -1;
+    public int uid => _playerUID;
+
     protected override void InitManager()
     {
    
@@ -55,6 +58,7 @@ public class AccountManager : Singleton<AccountManager>
         if (DBConnector.Instance.F_Insert(query))
         {
             Debug.Log("등록 성공 : ID[" + v_id + "] / PW[" + v_pw + "]");
+            F_CreateNewGameData(v_table, v_id);
             return true;
         }
         return false;
@@ -74,5 +78,29 @@ public class AccountManager : Singleton<AccountManager>
             return true;
 
         return false;
+    }
+
+    /// <summary>
+    ///  새로운 계정의 게임 데이터를 생성.
+    /// </summary>
+    /// <param name="v_table"></param>
+    /// <param name="v_id"></param>
+    private void F_CreateNewGameData(string v_table, string v_id)
+    {
+        string query = string.Format("SELECT * FROM {0} WHERE ID = '{1}'"
+        , v_table, v_id);
+
+        DataSet data = DBConnector.Instance.F_Select(query, v_table);
+        foreach(DataRow row in data.Tables[0].Rows)
+        {
+            int uid = int.Parse(row["UID"].ToString());
+
+            query = "INSERT INTO gamedata(UID) VALUES(" + uid + ")";
+            if(DBConnector.Instance.F_Insert(query))
+            {
+                Debug.Log("게임 데이터 생성 성공 UID : " + uid);
+            }
+            Debug.Log("게임 데이터 생성 실패");
+        }
     }
 }
