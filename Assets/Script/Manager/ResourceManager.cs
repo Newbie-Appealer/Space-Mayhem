669c24c5b0209888a7b4bbfed29df0d2578351d7
kaffu0424 <kaffu@naver.com>
2024-05-02 18:31:15 +0900

    ResourceManager 이펙트 관리 추가
    - 터렛에의한 운석 파괴시 이펙트 추가
      - ResourceManager를 통한 이펙트 관리
      - 오브젝트 풀링 적용
    - 이펙트 폴더 추가

--- Assets/Script/Manager/ResourceManager.cs
+++ Assets/Script/Manager/ResourceManager.cs
@@ -2,7 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using UnityEditor.Search;
 using UnityEngine;
+
+public enum EffectType
+{
+    EXPLOSION
+}
+
+class EffectBundle
+{
+    public GameObject _effectObject;
+    public ParticleSystem _effectParticle;
+
+    public EffectBundle(GameObject v_effectObject, ParticleSystem v_effectParticle)
+    {
+        _effectObject = v_effectObject;
+        _effectParticle = v_effectParticle;
+    }
+
+    /// <summary>
+    /// v_effectPosition 위치에 이펙트를 실행시킴
+    /// </summary>
+    /// <param name="v_effectPosition"></param>
+    public void F_UseEffect(Vector3 v_effectPosition)
+    {
+        _effectObject.transform.position = v_effectPosition;
+        _effectParticle.Play();
+    }
+}
 
 public class ResourceManager : Singleton<ResourceManager>
 {
@@ -19,10 +47,13 @@ public class ResourceManager : Singleton<ResourceManager>
 
 
     [Header("FX")]
-    private ParticleSystem _YEYEYEYEYAYAYAYEYEYAYEYAYEYAYEYAEYAYEYAYEYAYEA;
+    [SerializeField] private Transform _particleTransform;
+    [SerializeField] private GameObject[] _particlePrefabs;
+    private List<Queue<EffectBundle>> _particlePooling;
     protected override void InitManager() 
     {
         F_InitIntercationTEXTData();
+        F_InitEffectObjects();
     }
 
     #region Sprite
@@ -61,7 +92,47 @@ public class ResourceManager : Singleton<ResourceManager>
     #endregion
 
     #region
-    // GameObject로 풀링 만들어두기
-    // 풀링하면서 파트클시스템 미리 GetComponent해두기
+    private void F_InitEffectObjects()
+    {
+        _particlePooling = new List<Queue<EffectBundle>>();
+
+        foreach (GameObject effect in _particlePrefabs)
+        {
+            Queue<EffectBundle> queue = new Queue<EffectBundle>();
+            for(int i = 0; i < 3; i++)
+            {
+                GameObject effectObject = Instantiate(effect, _particleTransform);
+                ParticleSystem effectParticleSystem = effectObject.GetComponent<ParticleSystem>();
+
+                queue.Enqueue(new EffectBundle(effectObject, effectParticleSystem));
+            }
+            _particlePooling.Add(queue);
+        }
+    }
+    
+    public void F_GetEffect(EffectType v_type, Vector3 v_position)
+    {
+        if (_particlePooling[(int)v_type].Count <= 0)
+        {
+            GameObject effectObject = Instantiate(_particlePrefabs[(int)v_type], _particleTransform);
+            ParticleSystem effectParticleSystem = effectObject.GetComponent<ParticleSystem>();
+
+            _particlePooling[(int)v_type].Enqueue(new EffectBundle(effectObject, effectParticleSystem));
+        }
+
+        EffectBundle effect = _particlePooling[(int)v_type].Dequeue();
+        Debug.Log("이펙트 시작");
+        effect.F_UseEffect(v_position);
+        StartCoroutine(C_ReturnEffect(effect, v_type));
+    }
+
+    IEnumerator C_ReturnEffect(EffectBundle v_effect, EffectType v_type)
+    {
+        while (v_effect._effectParticle.isPlaying)  //실행되고있는동안 지연
+            yield return null;
+
+        Debug.Log("이펙트 종료");
+        _particlePooling[(int)v_type].Enqueue(v_effect);
+    }
     #endregion
 }
